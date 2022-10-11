6bf4bdb1f4264790082424a22c297937ef4c601d
Boris Veytsman <bveytsman@chanzuckerberg.com>
2022-10-11 13:43:07 -0700

    Changed test env

--- jest.config.ts
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  testEnvironment: "./node_modules/jsdom",
-  moduleDirectories: ["node_modules", "src"],
-  moduleNameMapper: {
-    ".(css|less|scss)$": "identity-obj-proxy",
-    "components/(.*)": "<rootDir>/src/components/$1",
-    "assets/(.*)": "<rootDir>/src/assets/$1",
-  },
-};
